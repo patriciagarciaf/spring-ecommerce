2f46ac77e6f2dbf6e40d2edc90912d1a1edf0b01
patriciagarciaf <patriciagarcia.fisica@gmail.com>
2022-10-09 21:18:47 +0200

    funcionalidad mostrar prod para admin

--- src/main/java/com/curso/ecommerce/service/ProductoService.java
+++ src/main/java/com/curso/ecommerce/service/ProductoService.java
@@ -1,5 +1,6 @@
 package com.curso.ecommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.curso.ecommerce.model.Producto;
@@ -14,4 +15,5 @@ public interface ProductoService {
 	
 	public void delete(Integer id);
 
+	public List<Producto> findAll();
 }
